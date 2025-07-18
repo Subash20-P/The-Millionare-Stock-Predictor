6a216870193a9fc9a53389867bf3fa8c8e523048
Subash20-P <subash200453@gmail.com>
2025-07-18 10:36:13 +0530

    mt5_config.py

--- The-Millionare-Stock-Predictor/mt5_config.py
+++ The-Millionare-Stock-Predictor/mt5_config.py
@@ -1,5 +1,5 @@
 # mt5_config.py
 
-LOGIN = 2271912549 # Your MT5 demo login
-PASSWORD = "sTrR?&jL?Bu*GE5"
-SERVER = "Exness-MT5Trial14"  # or whatever your broker's demo server name is
+LOGIN = ENTER YOUR ID # Your MT5 demo login
+PASSWORD = "ENTER YOUR PASSWORD"
+SERVER = "  # or whatever your broker's demo server name is"
